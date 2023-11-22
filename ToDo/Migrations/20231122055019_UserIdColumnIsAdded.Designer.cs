50d6635b893d78b5e06e37790911c740c270f94e
berdikulov-571 <mygithub571@gmail.com>
2023-11-22 12:50:06 +0500

    Migrations

--- ToDo/Migrations/20231122055019_UserIdColumnIsAdded.Designer.cs
+++ ToDo/Migrations/20231122055019_UserIdColumnIsAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDo.DataContext;
 
@@ -11,9 +12,11 @@ using ToDo.DataContext;
 namespace ToDo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231122055019_UserIdColumnIsAdded")]
+    partial class UserIdColumnIsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ToDo/Migrations/20231122055019_UserIdColumnIsAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserIdColumnIsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "ToDo",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ToDo");
+        }
+    }
+}
